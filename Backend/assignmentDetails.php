b096a0d7b6dd05801da9617e11084fa9219e6cbb
MohammadFakih02 <mohammadfakih025@gmial.com>
2024-11-18 19:20:44 +0200

    added api to view individual assignmenet

--- Backend/assignmentDetails.php
+++ Backend/assignmentDetails.php
@@ -8,3 +8,45 @@ if (!isset($userData['role'])) {
     echo json_encode(["error" => "Access denied: not logged in"]);
     exit;
 }
+
+if (isset($_GET["assignment_id"])) {
+    $assignment_id = $_GET["assignment_id"];
+
+    $sql = $connection->prepare("
+        SELECT 
+            assignments.assignment_id, 
+            users.username AS instructor_name, 
+            assignments.title, 
+            assignments.created_at, 
+            assignments.deadline, 
+            assignments.description
+        FROM 
+            users 
+        JOIN 
+            assignments 
+        ON 
+            users.user_id = assignments.instructor_id
+        WHERE 
+            assignments.assignment_id = ?
+    ");
+
+    $sql->bind_param("i", $assignment_id);
+
+    if ($sql->execute()) {
+        $result = $sql->get_result();
+
+        if ($result->num_rows > 0) {
+            $assignment = $result->fetch_assoc();
+            echo json_encode(["success" => true, "data" => $assignment]);
+        } else {
+            http_response_code(404);
+            echo json_encode(["error" => "Assignment not found"]);
+        }
+    } else {
+        http_response_code(500);
+        echo json_encode(["error" => "Unable to execute query"]);
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(["error" => "Assignment ID needed"]);
+}
